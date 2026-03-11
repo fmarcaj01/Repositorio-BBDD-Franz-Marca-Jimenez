4eabb421b253693383de5fdf26b0048ef87672f1
Hola Mundo <franzmarcajimenez@gmail.com>
2026-03-11 20:47:36 +0100

    AC602

--- Temas/UT6/AC602.sql
+++ Temas/UT6/AC602.sql
@@ -1,23 +1,27 @@
 -- 1 Inserción Ignorando Errores
-INSERT IGNORE INTO clientes (nombre, email, saldo) 
-VALUES ('Ana García', 'ana@email.com', 0.00);
+UPDATE productos 
+SET precio = 12.50 
+WHERE nombre = 'Ratón Óptico X1';
 
 -- 2 Actualizar al Insertar
-INSERT INTO clientes (nombre, email, saldo) 
-VALUES ('Luis Perez', 'luis@email.com', 5.00)
-ON DUPLICATE KEY UPDATE saldo = saldo + 5.00;
+UPDATE productos 
+SET precio = precio * 1.05 
+WHERE precio < 50;
 
 -- 3 Inserción con Sintaxis
-INSERT INTO productos 
-SET nombre = 'Auriculares Inalámbricos', 
-    precio = 29.99, 
-    stock = 15, 
-    proveedor_id = 2;
+update proveedores p 
+set nombre ='GlobalTech SA', ciudad='Tarragona'
+where nombre='InforGlobal'
 -- 4
--- Insertamos el nuevo proveedor
-INSERT INTO proveedores (nombre, ciudad) 
-VALUES ('Componentes Sur', 'Sevilla');
+UPDATE clientes 
+SET saldo = saldo * 1.10 
+WHERE saldo > 100;
 
--- Insertamos el producto asociado usando el ID recién creado
-INSERT INTO productos (nombre, precio, stock, proveedor_id) 
-VALUES ('Cable HDMI 2M', 8.50, 30, LAST_INSERT_ID());
+-- 5 
+UPDATE productos 
+SET precio = precio * 1.15 
+WHERE id_proveedor = (
+    SELECT id 
+    FROM proveedores 
+    WHERE nombre = 'ElectroComponentes'
+);
